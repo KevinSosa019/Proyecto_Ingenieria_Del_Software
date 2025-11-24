0b616068c537bc5a8141b69381178d1668c558ed
Raye1998 <127741466+Raye1998@users.noreply.github.com>
2025-11-23 21:19:10 -0600

    Delete PokerTech directory

--- PokerTech/pokertech/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Indica qué archivo de configuración debe usar Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokertech.settings')
-
-# Punto de entrada WSGI para servidores como Gunicorn / Apache
-application = get_wsgi_application()
